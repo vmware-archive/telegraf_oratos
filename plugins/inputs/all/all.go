56ea042fe246db9ec67e86b382642c6513a9a70f
Lyon Hill <lyondhill@gmail.com>
2019-05-21 14:29:05 -0700

    add to all

--- plugins/inputs/all/all.go
+++ plugins/inputs/all/all.go
@@ -119,6 +119,7 @@ import (
 	_ "github.com/influxdata/telegraf/plugins/inputs/redis"
 	_ "github.com/influxdata/telegraf/plugins/inputs/rethinkdb"
 	_ "github.com/influxdata/telegraf/plugins/inputs/riak"
+	_ "github.com/influxdata/telegraf/plugins/inputs/rss"
 	_ "github.com/influxdata/telegraf/plugins/inputs/salesforce"
 	_ "github.com/influxdata/telegraf/plugins/inputs/sensors"
 	_ "github.com/influxdata/telegraf/plugins/inputs/smart"
